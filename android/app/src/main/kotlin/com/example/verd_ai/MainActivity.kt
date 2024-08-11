c4190c0b8ec8528be007f04905b33001db9fdf67
yosra-wanen <yosra.wanen@e-iagen.com>
2024-08-11 20:17:22 +0100

    test appcenter

--- android/app/src/main/kotlin/com/example/verd_ai/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/verd_ai/MainActivity.kt
@@ -1,15 +1,5 @@
 package com.example.verd_ai
 
 import io.flutter.embedding.android.FlutterActivity
-import com.microsoft.appcenter.AppCenter;
-import com.microsoft.appcenter.analytics.Analytics;
-import com.microsoft.appcenter.crashes.Crashes;
 
-class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        
-        AppCenter.start(getApplication(), "2174cc5f-e585-417b-bc66-e018a54c25bf",
-                        Analytics::class.java, Crashes::class.java)
-    }
-}
+class MainActivity: FlutterActivity()
